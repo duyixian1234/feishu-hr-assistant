4449044c9fe788e9b6ce531cd314219350701a07
duyixian <duyixian@meideng.net>
2023-02-02 10:47:14 +0800

    日志输出到标准输出

--- main.py
+++ main.py
@@ -12,7 +12,6 @@ from events import process_event
 logging.basicConfig(
     level=logging.INFO,
     format="[%(asctime)s] [%(levelname)s] %(message)s",
-    handlers=[logging.FileHandler("./log/app.log")],
 )
 logger = logging.getLogger(__name__)
 storage.init_db()
